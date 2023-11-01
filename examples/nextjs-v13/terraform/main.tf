0836634c72ba8e798bb55c157d7c3c72a506dfb2
Eleftherios Myriounis <e.myriounis.@nexode.de>
2023-11-01 15:43:42 +0200

    update License for packages

--- examples/nextjs-v13/terraform/main.tf
+++ examples/nextjs-v13/terraform/main.tf
@@ -1,7 +1,7 @@
 module "next_serverless" {
   source = "../../../"
   # source  = "Nexode-Consulting/nextjs-serverless/aws"
-  # version = "0.2.9"
+  # version = "0.2.10"
 
   deployment_name = var.deployment_name
   region          = var.region
