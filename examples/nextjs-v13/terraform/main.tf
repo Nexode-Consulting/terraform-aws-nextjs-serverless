cc2a714e60f88cdf2f4ef8db1ba9d1d10bfeeec5
emyriounis <68233954+emyriounis@users.noreply.github.com>
2023-11-01 14:46:42 +0200

    update documentation (#22)
    * pump version
    
    * docs improvements
    
    ---------
    
    Co-authored-by: Eleftherios Myriounis <e.myriounis.@nexode.de>

--- examples/nextjs-v13/terraform/main.tf
+++ examples/nextjs-v13/terraform/main.tf
@@ -1,7 +1,7 @@
 module "next_serverless" {
   source = "../../../"
   # source  = "Nexode-Consulting/nextjs-serverless/aws"
-  # version = "0.2.7"
+  # version = "0.2.8"
 
   deployment_name = var.deployment_name
   region          = var.region
