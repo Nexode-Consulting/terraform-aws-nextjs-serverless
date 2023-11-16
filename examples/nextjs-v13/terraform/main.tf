0a0b461bf30a8521c72fc890dff1230b2150d9b4
emyriounis <68233954+emyriounis@users.noreply.github.com>
2023-11-16 20:02:22 +0200

    bugfix: image redirection & other minor staff (#34)
    * bugfix imageridirection
    
    * add known issue
    
    * update known issue
    
    ---------
    
    Co-authored-by: Eleftherios Myriounis <e.myriounis.@nexode.de>

--- examples/nextjs-v13/terraform/main.tf
+++ examples/nextjs-v13/terraform/main.tf
@@ -1,7 +1,7 @@
 module "next_serverless" {
   source = "../../../"
   # source  = "Nexode-Consulting/nextjs-serverless/aws"
-  # version = "0.2.12"
+  # version = "0.2.13"
 
   deployment_name = var.deployment_name
   region          = var.region
