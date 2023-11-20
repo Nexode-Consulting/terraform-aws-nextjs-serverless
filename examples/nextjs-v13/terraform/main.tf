31fb13d6dc3d6c57948bd1bc1e5fc02808dc19b5
Eleftherios Myriounis <e.myriounis.@nexode.de>
2023-11-20 19:38:15 +0200

    update docs & minor version

--- examples/nextjs-v13/terraform/main.tf
+++ examples/nextjs-v13/terraform/main.tf
@@ -1,7 +1,7 @@
 module "next_serverless" {
   source = "../../../"
   # source  = "Nexode-Consulting/nextjs-serverless/aws"
-  # version = "0.2.14"
+  # version = "0.2.15"
 
   deployment_name = var.deployment_name
   region          = var.region
