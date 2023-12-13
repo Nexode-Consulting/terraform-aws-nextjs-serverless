c0fe4e893162f8f1ed148ad6e701fbcc329ac2d8
Eleftherios Myriounis <e.myriounis.@nexode.de>
2023-12-13 19:14:48 +0200

    Fix broken SPA feature

--- packages/build-serverless-next/server.js
+++ packages/build-serverless-next/server.js
@@ -1,4 +1,37 @@
 'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
 var __importDefault =
   (this && this.__importDefault) ||
   function (mod) {
@@ -10,16 +43,37 @@ const next_server_1 = __importDefault(require('next/dist/server/next-server'))
 const serverless_http_1 = __importDefault(require('serverless-http'))
 // @ts-ignore
 const required_server_files_json_1 = require('./.next/required-server-files.json')
+const getProps = (event, context) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const path =
+      './.next/server/pages/' +
+      event.rawPath
+        .replace('/_next/data/', '')
+        .split('/')
+        .slice(1)
+        .join('/')
+        .replace('.json', '.js')
+    const { getServerSideProps } = require(path)
+    const customResponse = yield getServerSideProps(context)
+    const response = {}
+    response.statusCode = 200
+    response.body = JSON.stringify({ pageProps: customResponse.props })
+    return response
+  })
 const nextServer = new next_server_1.default({
   hostname: 'localhost',
   port: 3000,
-  dir: __dirname,
+  dir: './',
   dev: false,
   conf: Object.assign({}, required_server_files_json_1.config),
+  customServer: true,
 })
-exports.handler = (0, serverless_http_1.default)(
-  nextServer.getRequestHandler(),
-  {
-    binary: ['*/*'],
-  }
-)
+const main = (0, serverless_http_1.default)(nextServer.getRequestHandler(), {
+  binary: ['*/*'],
+  provider: 'aws',
+})
+const handler = (event, context) =>
+  event.rawPath.includes('/_next/data/')
+    ? getProps(event, context)
+    : main(event, context)
+exports.handler = handler
--- packages/build-serverless-next/server.ts
+++ packages/build-serverless-next/server.ts
@@ -1,21 +1,46 @@
-import { NextConfig } from "next";
-import NextServer from "next/dist/server/next-server";
-import serverless from "serverless-http";
+import { NextConfig } from 'next'
+import NextServer from 'next/dist/server/next-server'
+import serverless from 'serverless-http'
 // @ts-ignore
-import { config } from "./.next/required-server-files.json";
+import { config } from './.next/required-server-files.json'
+
+const getProps = async (event: any, context: any) => {
+  const path =
+    './.next/server/pages/' +
+    event.rawPath
+      .replace('/_next/data/', '')
+      .split('/')
+      .slice(1)
+      .join('/')
+      .replace('.json', '.js')
+  const { getServerSideProps } = require(path)
+
+  const customResponse = await getServerSideProps(context)
+  const response: any = {}
+
+  response.statusCode = 200
+  response.body = JSON.stringify({ pageProps: customResponse.props })
+
+  return response
+}
 
 const nextServer = new NextServer({
-  hostname: "localhost",
+  hostname: 'localhost',
   port: 3000,
   dir: './',
   dev: false,
   conf: {
     ...(config as NextConfig),
   },
-});
+  customServer: true,
+})
 
-export const handler = serverless(nextServer.getRequestHandler(), {
-  binary: ["*/*"],
+const main = serverless(nextServer.getRequestHandler(), {
+  binary: ['*/*'],
   provider: 'aws',
-  basePath: './'
-});
+})
+
+export const handler = (event: any, context: any) =>
+  event.rawPath.includes('/_next/data/')
+    ? getProps(event, context)
+    : main(event, context)
